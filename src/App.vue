469504c65f395f9adb8ff22eaf4c743d68850afd
jiyoon0114 <greenpkh02@naver.com>
2026-08-07 18:29:37 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from './components/Header.vue';
+import UserInfo from './components/UserInfo.vue';
+import PostIndex from './pages/posts/PostIndex.vue';
+</script>
+
+<template>
+  <Header />
+  <UserInfo />
+  <router-view></router-view>
+</template>
+
+<style scoped>
+</style>
